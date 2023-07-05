6094d2053bbbb22b3ea0901d661598d3d2fbe0c6
Mihlali-Ma <129970639+Mihlali-Ma@users.noreply.github.com>
2023-07-05 12:39:08 +0200

    needs to be fixed

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="about">
     <NavBar/>
+    <FooterComp/>
+    <footeR/>
   </div>
 </template>
 
 <script>
+import FooterComp from '../components/FooterComp.vue';
   import NavBar from '../components/NavBar';
+import footeR from '../components/FooteR.vue'
   export default {
-    components: { NavBar }
+    components: { NavBar,FooterComp,footeR}
 }
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,8 @@
     <NavBar/>
     <img src="https://i.postimg.cc/rwK59hbb/img-nature-wide.jpg" class="img-fluid" alt="...">
     <MyWork/>
+    <footer-comp/>
+    <FooteR/>
 
   </div>
 </template>
@@ -11,12 +13,16 @@
 // @ is an alias to /src
 import NavBar from '@/components/NavBar';
 import MyWork from '@/components/MyWork.vue';
+import FooterComp from '@/components/FooterComp.vue';
+import FooteR from '@/components/FooteR.vue';
 
 export default {
   name: 'HomeView',
   components: {
     NavBar,
-    MyWork
+    MyWork,
+    FooterComp,
+    FooteR
   }
 }
 </script>
