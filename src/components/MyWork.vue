5871cbba36af4f36075f0b0332d1cfbe87f78ca3
Mihlali-Ma <129970639+Mihlali-Ma@users.noreply.github.com>
2023-07-07 12:04:52 +0200

    work

--- src/components/MyWork.vue
+++ src/components/MyWork.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
+    <div class="body">
         <br>
         <H2 id="heading">My Work</H2>
         <div class="row">
-            <div class="col" v-for="project in projects" :key="project.name">
+            <div class="col-4 md-8" v-for="project in projects" :key="project.name">
                 <div class="card" style="width: 18rem;">
-                    <img :src="project.image" class="card-img-top" :alt="project.name">
+                    <img :src="project.Image" class="card-img-top" :alt="project.name">
                     <div class="card-body">
                       <h5 class="card-title">{{ project.name }}</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                      <p class="card-text">{{ project.Description }}</p>
+                      <a :href="project.github" class="btn btn-primary">Github</a>
+                      <a :href="project.netlify" class="btn btn-primary">netlify</a>
+
                     </div>
                   </div>
             </div>
@@ -32,5 +34,24 @@
 </script>
 
 <style scoped>
-    
+    .row{
+        align-content: center;
+        padding: 0rem;
+    }
+    .col-4{
+        margin-bottom: 5rem;
+       /* margin-left:5rem ;*/
+        /*margin-right: -5rem;*/
+        align-content: center;
+        color:white;
+    }
+    .card{
+        width: 100%;
+        background: #181818;
+        border: none;
+        box-shadow: 0px 0px 25px black;
+    }
+    .col h5,p{
+        color: #fff;
+    }
 </style>
